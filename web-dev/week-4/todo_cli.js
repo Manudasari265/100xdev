e2f19782fe14d95b5a3ffae0083442c5a2ee5388
Manoj Dasari <dasarimanoj265@gmail.com>
2024-09-02 23:58:08 +0530

    updated the CLI-todo

--- web-dev/week-4/todo_cli.js
+++ web-dev/week-4/todo_cli.js
@@ -1,51 +1,62 @@
-const { default: chalk } = require('chalk');
-const { commander } = require('commander');
+const fs = require('fs');
+import { Command } from 'commander';
+import chalk from 'chalk';
+
 const program = new Command();
 
 program
-  .name('CLI Based Todo')
-  .description('CLI to manage your tasks')
-  .version('0.12.1');
+  .name('todo')
+  .description('CLI to manage your todo list')
+  .version('0.1.0');
 
+// Command to list all tasks
 program.command('list')
   .description('Show all tasks')
-  .action( () => {
+  .action(() => {
     showTasks();
-  })
+  });
 
+// Command to add a new task
 program.command('add')
   .description('Add a new task')
   .argument('<task>', 'The task to add')
-  .action( (task) => {
+  .action((task) => {
     addTask(task);
-  })
+  });
 
 function showTasks() {
-    const tasks = loadTasks();
-    if(tasks.length === 0){
-        console.log(chalk.red('No tasks to show'));
-    } else {
-        console.log(chalk.green('Your tasks: '));
-        todos.forEach(task, index => {
-            const status = task.done ? chalk.green('[✓]') : chalk.red('[]');
-            console.log(chalk.bgGreenBright(`${index + 1}. ${task.name}`));
-        });
-    }
+  const tasks = loadTasks();
+  if (tasks.length === 0) {
+    console.log(chalk.yellow('No tasks to show.'));
+  } else {
+    console.log(chalk.green('Your tasks:'));
+    tasks.forEach((task, index) => {
+      const status = task.done ? chalk.green('[✓]') : chalk.red('[ ]');
+      console.log(`${index + 1}. ${status} ${task.name}`);
+    });
+  }
 }
 
 function addTask(task) {
-    const todos = loadTasks();
-    todos.push({ name: task, done: false});
-    fs.writeFile('todos.json', JSON.stringify(t))
+  const tasks = loadTasks();
+  tasks.push({ name: task, done: false });
+  saveTasks(tasks);
+  console.log(chalk.green(`Added task: ${task}`));
 }
 
+// Helper function to load tasks from tasks.json
 function loadTasks() {
-    try {
-        const data = fs.readFile('todos.json', 'utf-8');
-        return JSON.parse(data);
-    } catch(err) {
-        return [];
-    }
+  try {
+    const data = fs.readFileSync('tasks.json', 'utf8');
+    return JSON.parse(data);
+  } catch (err) {
+    return [];
+  }
+}
+
+// Helper function to save tasks to tasks.json
+function saveTasks(tasks) {
+  fs.writeFileSync('tasks.json', JSON.stringify(tasks, null, 2));
 }
 
 program.parse(process.argv);
