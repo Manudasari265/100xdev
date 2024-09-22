f7667e1904f11bded3c7d15a8112fa82e97d784d
Manoj Dasari <dasarimanoj265@gmail.com>
2024-09-22 21:14:55 +0530

    cleaned up some of the environment variables

--- web-dev/week-8/server.js
+++ web-dev/week-8/server.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 const jwt = require('jsonwebtoken');
 const { userRouter } = require('./routes/user');
 const { courseRouter } = require('./routes/course');
-
+require('dotenv').config();
 
 app.use(express.json());
 app.use("/user", userRouter);
@@ -15,7 +15,7 @@ app.get()
 
 async function main() {
    // ! secure the db connection string before remote commit
-   await mongoose.connect("");
+   await mongoose.connect(process.env.DATABASE_URL);
    app.listen(PORT, () => {
       console.log(`Server is running on ${PORT}`);
    });
