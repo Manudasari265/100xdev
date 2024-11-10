2324f46377fe0ea575964d264f3c76daf7831a76
Manoj Dasari <dasarimanoj265@gmail.com>
2024-11-10 22:16:21 +0530

    arrays in ts

--- web-dev/week-14/typescript-2/src/arrays.ts
+++ web-dev/week-14/typescript-2/src/arrays.ts
@@ -11,3 +11,15 @@ function getMax(nums: number[]) {
 }
 
 getMax([2,3,4,5]);
+
+interface User {
+    firstName: string;
+    lastName: string;
+    age: number;
+}
+
+function filteredUsers(users: User[]) {
+    return users.filter(user => user.age > 18);
+}
+
+
