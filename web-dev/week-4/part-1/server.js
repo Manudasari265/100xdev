b3e4a972f6b56541d58853ed444909235898409e
Manoj Dasari <dasarimanoj265@gmail.com>
2024-11-11 23:19:52 +0530

    advanced backend

---
+++ web-dev/week-4/part-1/server.js
@@ -0,0 +1,14 @@
+// Middleware to parse JSON request bodies
+// Example route handlers
+// Save user to database
+// Example middleware to log requests
+// Example custom error handling middleware
+
+const express = require('express');
+
+const app = express;
+
+app.get("/", function(req, res) {
+
+})
+
