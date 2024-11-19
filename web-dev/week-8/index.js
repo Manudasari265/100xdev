691fb57ce4cbe9e34f0a8618ddc2503c1947bd6f
Manoj Dasari <dasarimanoj265@gmail.com>
2024-11-19 23:18:50 +0530

    zod && bcrypt password hashing

--- web-dev/week-8/index.js
+++ web-dev/week-8/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { userModel } from './schema/db';
+import { z } from 'zod';
 
 const app = express();
 const PORT = 3001;
@@ -7,18 +8,39 @@ const PORT = 3001;
 app.use(express.json());
 
 app.post("/signup", async function (req, res) {
+    const requiredBody = z.object({
+        email: z.string().min(5).max(100).email(),
+        password: z.string().min(5).max(100),
+        name: z.string().min(5).max(100)
+    })
+
+    const parsedDataWithSuccess = requiredBody.safeParse(req.body);
+    if(!parsedDataWithSuccess.success) {
+        res.status(403).json({
+            message: "Incorrect format",
+            error: parsedDataWithSuccess.error,
+        })
+        return;
+    }
     try {
         const { email } = req.body;
         const { password } = req.body;
         const { name } = req.body;
 
-        const hashedPassword = 
-        await userModel.create({
+        const hashedPassword = await bcrypt.hash(password, 5);
 
+        await userModel.create({
+            email: email,
+            password: hashedPassword,
+            name: name,
+        })
+
+        res.status(201).json({
+            message: "You are signed up"
         })
     } catch (error) {
-        
+        res.status(500).json({
+            error: "Internal server error",
+        })
     }
-
-
 })
