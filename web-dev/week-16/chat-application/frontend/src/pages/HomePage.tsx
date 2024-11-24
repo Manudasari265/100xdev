ded3b1730159d9790206200c6fbce6028cd6ce25
Manoj Dasari <dasarimanoj265@gmail.com>
2024-11-24 22:54:32 +0530

    fix: ws-connect error fixed

--- web-dev/week-16/chat-application/frontend/src/pages/HomePage.tsx
+++ web-dev/week-16/chat-application/frontend/src/pages/HomePage.tsx
@@ -1,23 +1,56 @@
-import { useEffect } from "react"
+
+import { useEffect, useRef, useState } from 'react'
 
 export default function HomePage() {
-    useEffect(() => {
-        const ws = new WebSocket("ws:localhost:8080");
-        ws.onopen = () => {
-            ws.send('Hello Server!');
-        }
-        ws.onmessage = (message) => {
-            console.log('Message received: ', message.data);
-        }
-    },[])
+  const [messages, setMessages] = useState(["hi there", "hello"]);
+  const wsRef = useRef();
+  const inputRef = useRef();
 
+  useEffect(() => {
+    const ws = new WebSocket("http://localhost:8080");
+    ws.onmessage = (event) => {
+      setMessages(m => [...m, event.data])
+    }
+    wsRef.current = ws;
 
-    return <div className="h-screen bg-black">
-        <div className="h-[95vh]"></div>
-        <div className="w-full bg-white flex">
-            <input className="flex-1 p-4" type="text" />
-            <button className="bg-purple-600 text-white p-4">Send message</button>
-        </div>
-        
+    ws.onopen = () => {
+      ws.send(JSON.stringify({
+        type: "join",
+        payload: {
+          roomId: "red"
+        }
+      }))
+    }
+    return () => {
+      ws.close()
+    }
+  }, []);
+
+  return (
+    <div className='h-screen bg-black'>
+      <br /><br /><br />
+      <div className='h-[85vh]'>
+        {messages.map(message => <div className='m-8'> 
+          <span className='bg-white text-black rounded p-4 '>            
+            {message} 
+          </span>
+        </div>)}
+      </div>
+      <div className='w-full bg-white flex'>
+        <input ref={inputRef} id="message" className="flex-1 p-4"></input>
+        <button onClick={() => {
+          const message = inputRef.current?.value;
+          wsRef.current.send(JSON.stringify({
+            type: "chat",
+            payload: {
+              message: message
+            }
+          }))
+
+        }} className='bg-purple-600 text-white p-4'>
+          Send message
+        </button>
+      </div>
     </div>
+  )
 }
