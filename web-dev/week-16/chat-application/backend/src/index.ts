f687d1c69a7cf6e0db190c6d6f858cc2821cdd3a
Manoj Dasari <dasarimanoj265@gmail.com>
2024-11-24 21:42:46 +0530

    feat: home-page ui

--- web-dev/week-16/chat-application/backend/src/index.ts
+++ web-dev/week-16/chat-application/backend/src/index.ts
@@ -1,23 +1,94 @@
-import { WebSocket, WebSocketServer } from "ws";
+// import { WebSocket, WebSocketServer } from "ws";
+
+// const wss = new WebSocketServer({ port: 8080 });
+
+// interface User {
+//     socket: WebSocket;
+//     room: string;
+// }
+
+// let userCount = 0;
+// let allSockets: User[] = []; //TODO: use MAPS, RECORDS here
+
+// wss.on('connection', (sockets: User) => {
+//     allSockets.push(sockets);
+
+//     userCount += 1;
+//     console.log(`User connected ${userCount}`);
+
+//      event handler - 'message'
+//     sockets.on("message", (message) => {
+//         const parsedMessage = JSON.parse(message as unknown as string);
+//         if(parsedMessage.type === "join") {
+//             allSockets.push({
+//                 socket,
+//                 room: parsedMessage.payload.roomId,
+//                 roomId
+//             })
+//         }
+
+//         if(parsedMessage.type === "chat") {
+//             const currentUserRoom = allSockets.find((x) => s.socket == sockets)?.room
+
+//             allSockets.forEach(userRoom => {
+
+//             })
+//         }
+//         console.log("message received: " + message.toString());
+
+//         sockets.send(message.toString() + " :sent from the server");
+//         allSockets.forEach(sockets => {
+//             sockets.send(message.toString() + ": sent from the server");
+//         })
+//     })
+
+//     sockets.on("disconnect", () => {
+//         allSockets = allSockets.filter(x => x != sockets);
+//     })
+//     //TODO Add room logic after this end-point
+// })
+
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-let userCount = 0;
-let allSockets: WebSocket[] = [];
+interface User {
+    socket: WebSocket;
+    room: string;
+}
 
-wss.on('connection', (socket: WebSocket) => {
-    allSockets.push(socket);
+let allSockets: User[] = [];
 
-    userCount += 1;
-    console.log(`User connected ${userCount}`);
+wss.on("connection", (socket) => {
 
-    // event handler - 'message'
     socket.on("message", (message) => {
-        console.log("message received: " + message.toString());
+        // @ts-ignore
+        const parsedMessage = JSON.parse(message);
+        if (parsedMessage.type == "join") {
+            console.log("user joined room " + parsedMessage.payload.roomId);
+            allSockets.push({
+                socket,
+                room: parsedMessage.payload.roomId
+            })
+        }
 
-        socket.send(message.toString() + " :sent from the server");
-        allSockets.forEach(sockets => {
-            sockets.send(message.toString() + ": sent from the server");
-        })
+        if (parsedMessage.type == "chat") {
+            console.log("user wants to chat");
+            // const currentUserRoom = allSockets.find((x) => x.socket == socket).room
+            let currentUserRoom = null;
+            for (let i = 0; i < allSockets.length; i++) {
+                if (allSockets[i].socket == socket) {
+                    currentUserRoom = allSockets[i].room
+                }
+            }
+
+            for (let i = 0; i < allSockets.length; i++) {
+                if (allSockets[i].room == currentUserRoom) {
+                    allSockets[i].socket.send(parsedMessage.payload.message)
+                }
+            }
+        }
+
     })
+
 })
--- web-dev/week-16/chat-application/frontend/src/pages/HomePage.tsx
+++ web-dev/week-16/chat-application/frontend/src/pages/HomePage.tsx
@@ -4,12 +4,20 @@ export default function HomePage() {
     useEffect(() => {
         const ws = new WebSocket("ws:localhost:8080");
         ws.onopen = () => {
-            
+            ws.send('Hello Server!');
+        }
+        ws.onmessage = (message) => {
+            console.log('Message received: ', message.data);
         }
     },[])
 
 
-    return <div>
-        hi there!
+    return <div className="h-screen bg-black">
+        <div className="h-[95vh]"></div>
+        <div className="w-full bg-white flex">
+            <input className="flex-1 p-4" type="text" />
+            <button className="bg-purple-600 text-white p-4">Send message</button>
+        </div>
+        
     </div>
 }
