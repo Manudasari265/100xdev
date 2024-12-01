063efc6fca55236786ade2cfc394d54797e3cedd
Manoj Dasari <dasarimanoj265@gmail.com>
2024-12-01 20:49:27 +0530

    sql relationships

--- web-dev/week-18/postgresSQL-1/src/index.ts
+++ web-dev/week-18/postgresSQL-1/src/index.ts
@@ -17,12 +17,25 @@ app.post("/signup", async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
     const email = req.body.email;
+    const city = req.body.city;
+    const country = req.body.country;
+    const street = req.body.street;
+    const pincode = req.body.pincode;
+
 
     try {
         const insertQuery = `INSERT INTO user 
           (username, email, password) VALUES 
-          (${username}, ${email}, ${password})`;
+          ($1, $2, $3) RETURNING id`;
+
         const response = await pgClient.query(insertQuery);
+        const userId = response.rows[0].id;
+        
+        const addressInsertQuery = `INSERT INTO addresses 
+          (city, country, street, pincode, user_id) VALUES 
+          ($1, $2, $3, $4, $5)`;
+        
+        const addressInsertResponse = await pgClient.query(addressInsertQuery, [city, country, street, pincode, userId]);
 
         res.json({
             message: "You have signed up",
