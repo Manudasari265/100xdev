1f573e84981be7ed5df629be53ba0126c6d0e4eb
Manoj Dasari <dasarimanoj265@gmail.com>
2024-12-27 22:00:24 +0530

    eth-backend init

---
+++ web3/week-21/backend-eth/src/index.ts
@@ -0,0 +1,12 @@
+import { JsonRpcProvider, id } from "ethers";
+
+const provider = new JsonRpcProvider("https://eth-mainnet.g.alchemy.com/v2/nnY0qPUQLYsUvb5BKJM5bh81sI6O0PQG");
+
+function pollBlock(blockNumber: number) {
+    provider.getLogs({
+        address: "0xdac17f958d2ee523a2206206994597c13d831ec7",
+        fromBlock: blockNumber,
+        toBlock: blockNumber,
+        topics: [id("Transfer(address,address,uint256)")],
+    })
+}
