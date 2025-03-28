d3595a33cc0b066c54127eb1f12e083dadd3aa14
Manoj Dasari <dasarimanoj265@gmail.com>
2025-03-28 21:46:26 +0530

    native rust testcases done

---
+++ web3/week-31/client/index.test.ts
@@ -0,0 +1,50 @@
+import {
+    Keypair,
+    Connection,
+    LAMPORTS_PER_SOL,
+    PublicKey,
+    SystemProgram,
+    Transaction,
+} from "@solana/web3.js";
+import { CounterAccount, schema, COUNTER_SIZE } from "./types/types";
+import * as borsh from "borsh";
+import { expect, test } from "bun:test";
+
+let counterAccount: Keypair = Keypair.generate();
+let adminAccount: Keypair = Keypair.generate();
+const connection = new Connection("http://localhost:8899", "confirmed");
+
+const programId = new PublicKey("");
+
+test("Account is initialized", async () => {
+    const txn = await connection.requestAirdrop(
+        adminAccount.publicKey,
+        2 * LAMPORTS_PER_SOL
+    );
+    await connection.confirmTransaction(txn);
+
+    const data = await connection.getAccountInfo(adminAccount.publicKey);
+    console.log(data);
+
+    const lamports = await connection.getMinimumBalanceForRentExemption(
+        COUNTER_SIZE
+    );
+
+    const createCounterAccountIx = SystemProgram.createAccount({
+        fromPubkey: adminAccount.publicKey,
+        lamports,
+        newAccountPubkey: counterAccount.publicKey,
+        programId: programId,
+        space: COUNTER_SIZE,
+    });
+
+    const createAccountTxn = new Transaction();
+    createAccountTxn.add(createCounterAccountIx);
+
+    const txHash = await connection.sendTransaction(createAccountTxn, [
+        adminAccount,
+        counterAccount,
+    ]);
+    
+    await connection.confirmTransaction(txHash);
+});
---
+++ web3/week-31/client/types/types.ts
@@ -0,0 +1,17 @@
+import * as borsh from "borsh";
+
+export class CounterAccount {
+    count = 0;
+
+    constructor({ count } : { count: number }) {
+        this.count = count;
+    }
+}
+
+export const schema: borsh.Schema = {
+    struct : {
+        count: 'u32'
+    }
+};
+
+export const COUNTER_SIZE = borsh.serialize(schema,new CounterAccount({ count: 0 })).length;
